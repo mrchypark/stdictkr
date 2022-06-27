ba3b531d960fe33a555c5957c46aa82f5ce4f25d
chanyub Park <mrchypark@gmail.com>
2022-06-27 22:55:42 +0900

    change to stdictkr

--- R/.logo.R
+++ R/.logo.R
@@ -15,13 +15,13 @@ library(hexSticker)
 sticker(
   "./man/figures/logo_crop.png",
   s_x = 1, s_y = 0.75,
-  package = "stdictapi",
+  package = "stdictkr",
   p_size = 45, p_y = 1.4, p_x = 1,
   filename = "man/figures/logo.png",
   h_fill = "#f8f8f0",
   p_color = "#222222",
   h_color = "#3f3936",
-  url = "mrchypark.github.io/stdictapi",
+  url = "mrchypark.github.io/stdictkr",
   u_size = 9,
   u_color = "#3f3936",
   dpi = 500
