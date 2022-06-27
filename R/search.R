0065f335e83749710afae1d776e5a580b483015e
chanyub Park <mrchypark@gmail.com>
2022-06-27 23:38:46 +0900

    raw change

--- R/search.R
+++ R/search.R
@@ -8,25 +8,25 @@
 #'
 #' @importFrom urltools param_set
 #' @importFrom httr GET status_code content
-#' @importFrom jsonlite fromJSON
+#' @importFrom xml2 as_list
 #' @importFrom keyring key_get
 #' @export
-std_search <- function(query, start = 1, count = 10) {
+std_search <- function(query,
+                       start = 1,
+                       count = 10) {
   "https://stdict.korean.go.kr/api/search.do" %>%
     urltools::param_set("key", std_key()) %>%
     urltools::param_set("q", query) %>%
-    urltools::param_set("req_type", "json") %>%
+    urltools::param_set("req_type", "xml") %>%
     urltools::param_set("start", start) %>%
     urltools::param_set("num", count) %>%
     httr::GET() -> resp
 
-    if (httr::status_code(resp) != 200) {
-      return("error")
-    }
+  if (httr::status_code(resp) != 200) {
+    return("error")
+  }
 
-    httr::content(resp, "text") %>%
-    jsonlite::fromJSON() -> res
+  httr::content(resp) %>%
+    xml2::as_list() -> res
   return(res$channel$item)
 }
-
-
