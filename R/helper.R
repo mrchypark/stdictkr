282e0dc24a1ed30d617d4ffebed03191e59ec777
chanyub Park <mrchypark@gmail.com>
2022-06-29 12:30:00 +0900

    open api key page

---
+++ R/helper.R
@@ -0,0 +1,13 @@
+#' this function from usthis:::view_url
+#' @importFrom utils browseURL
+#' @importFrom usethis ui_todo ui_done
+view_url <- function(..., open = interactive()) {
+  url <- paste(..., sep = "/")
+  if (open) {
+    ui_done("Opening URL {url}")
+    utils::browseURL(url)
+  } else {
+    ui_todo("Open URL {url}")
+  }
+  invisible(url)
+}
